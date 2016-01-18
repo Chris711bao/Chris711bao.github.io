469cf5a4a097a0960748a17b5ce9e9368b36bed7
Chris711bao <814889470@qq.com>
2016-01-18 21:51:46 +0800

    线程池可编译
    Signed-off-by: Chris711bao <814889470@qq.com>

---
+++ ThreadPool/ThreadPool/stdafx.h
@@ -0,0 +1,23 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+#include <iostream>
+#include <string>
+#include "threadpoolmanager.h"
+#include <string.h>
+#include <stdlib.h>
+#include "..\include\pthread.h"
+#include "commondef.h"
+
+#include "windows.h"
+#pragma comment(lib, "..\\lib\\x86\\pthreadVC2.lib")
+// TODO: 在此处引用程序需要的其他头文件
